ec3cd3c3d2b804a8b0b408e5f8dd741281b9bf1a
CODECLOUDS-AMITM <codeclouds-amitm@CISPL-IO-730.local>
2024-02-25 23:19:32 +0530

    add new file

---
+++ domSelectJS.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// ==========appendChild===========
+// let creaname = document.createElement("h2");
+// creaname.textContent = "This is My text";
+
+// let demoSection = document.getElementById("demo");
+// demoSection.appendChild(creaname);
+
+
+// ==========insertBefore===========
+// let creaname = document.createElement("h2");
+// creaname.textContent = "This is My text";
+
+// let demoSection = document.getElementById("demo");
+// demoSection.appendChild(creaname);
+
+// demoSection.insertBefore(creaname, demoSection.childNodes[0])
+
+
+// ==========replaceChild===========
+
+// let replaceEle = document.createElement("h5");
+// let replaceEleId = document.createTextNode("My name is Amit");
+
+// replaceEle.appendChild(replaceEleId);
+// console.log(replaceEle)
+
+// let parentid =document.getElementById("demo");
+
+// let oldChild = parentid;
+// console.log(oldChild)
+
+// parentid.replaceChild(replaceEle, oldChild.childNodes[3])
+
+
+let iheight =window.innerHeight;
+let divheight = document.getElementById("demo");
+
+
+
+
+
+
+
+
