79af8e427aafc2ce2bd104eb8b22905c33398800
CODECLOUDS-AMITM <codeclouds-amitm@CISPL-IO-730.local>
2024-03-10 20:02:00 +0530

    updated files

--- domSelectJS.js
+++ domSelectJS.js
@@ -34,8 +34,79 @@
 // parentid.replaceChild(replaceEle, oldChild.childNodes[3])
 
 
-let iheight =window.innerHeight;
-let divheight = document.getElementById("demo");
+
+// let elementN =document.createElement("li");
+// let changesVal = document.createTextNode("2. Second");
+
+// elementN.appendChild(changesVal);
+// console.log(elementN);
+
+// let listIteam = document.getElementById("list");
+
+// let oldIteam = listIteam;
+// console.log(oldIteam);
+
+// oldIteam.replaceChild(elementN, oldIteam.childNodes[3])
+
+
+// ==========height===========
+//let iheight =window.innerHeight + "px";
+
+// let divheight = document.getElementById("demo");
+// divheight.style.height = "500" + "px";
+// console.log(divheight)
+// ======================
+
+
+// ==========remove===========
+// let removeid = document.querySelector("#demo h5");
+// removeid.remove()
+// console.log(removeid)
+
+
+// ==========DOM CSS(Style, className, classList)===========
+// let selectId = document.querySelector("#demo").style.color= "white";
+// console.log(selectId)
+
+// let selectId =document.querySelector("#demo").className = "xyz";
+// console.log(selectId)
+
+// let selectId =document.querySelector("#demo").classList.add("xyz", "newClass");
+// console.log(selectId)
+
+
+// ==========querySelectorAll,querySelector===========
+
+// let querySelect = document.querySelectorAll("#demo h5")[1].innerHTML="Lorem ipsum dolor";
+// console.log(querySelect)
+
+// let querySelect = document.querySelector("#demo h5").innerHTML="Lorem ipsum dolor";//(select to first one)
+// console.log(querySelect)
+
+// ==========querySelectorAll,querySelector===========
+
+
+let sho = document.getElementById("Show_text").style.display="none";
+let button = document.querySelector("#click");
+
+button.addEventListener("click", function(){
+    let sho =document.getElementById("Show_text");
+    if(sho.style.display === "none"){
+      sho.style.display = "block";
+      console.log(sho.style.display)
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 
